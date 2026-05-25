308b4ed115fa37f5fb642b55f91ffe7a92d92ff2
favas-cv <fawazmuhd06@example.com>
2026-05-25 15:59:32 +0530

    production setup updated

--- rentout_core/config/config/settings/base.py
+++ rentout_core/config/config/settings/base.py
@@ -92,6 +92,7 @@ DATABASES = {
         "PASSWORD": config("DB_PASSWORD"),
         "HOST": config("DB_HOST"),
         "PORT": config("DB_PORT"),
+        "CONN_MAX_AGE": 60,
     }
 }
 
--- rentout_core/config/config/settings/production.py
+++ rentout_core/config/config/settings/production.py
@@ -19,7 +19,6 @@ CSRF_TRUSTED_ORIGINS = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    "https://yourfrontend.vercel.app",
     "http://localhost:5173",
     "https://rentoutindia.fawazmuhd06.workers.dev",
 ]
@@ -32,8 +31,8 @@ CORS_ALLOW_CREDENTIALS = True
 
 SECURE_SSL_REDIRECT = False #change  wehn  vps iwht https 
 
-SESSION_COOKIE_SECURE = False
-CSRF_COOKIE_SECURE = False
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 
 SECURE_BROWSER_XSS_FILTER = True
 SECURE_CONTENT_TYPE_NOSNIFF = True
@@ -42,3 +41,19 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+
+# log production
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "INFO",
+    },
+}
